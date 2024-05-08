f6ca6e12ace003969db3fd83ab93ce043eb66105
Talha Wajid <100958239+talha765@users.noreply.github.com>
2024-05-08 23:50:28 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,12 +1,12 @@
-import 'dart:io' as io;
 import 'dart:io';
+import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
-// import 'package:file_picker/file_picker.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 import 'package:image_picker/image_picker.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 void main() {
   runApp(MyApp());
@@ -34,18 +34,14 @@ class _HomePageState extends State<HomePage> {
   String recognizedFaces = '';
   String errorMessage = '';
   Uint8List? output;
+  List<String> recognizedNames = [];
 
   Future<void> pickImage() async {
     final picker = ImagePicker();
     final res = await picker.pickImage(source: ImageSource.gallery);
 
-    // print(result);
-
     if (res != null) {
-      // setState(() {
       _image = File(res.path);
-      // _image = File(result.files[0].path!);
-      // });
       await recognizeFaces();
     } else {
       print('No image selected.');
@@ -53,33 +49,17 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> recognizeFaces() async {
-    const apiUrl = 'http://192.168.1.106:5000/recognize';
-    print(apiUrl);
+    const apiUrl = 'http://10.1.144.71:5000/recognize';
     String img;
 
     if (_image != null) {
       Uint8List fileBytes = _image!.readAsBytesSync();
-      // print(fileBytes);
-
       var fparts = _image!.path.split('/').last.split('.');
       var ext = fparts.last;
-      print(ext);
-
-      img = 'data:image/${ext};base64,${base64Encode(fileBytes)}';
-      print(img);
-      // Now you can use img which contains the base64 encoded string
-
-      // final bytes = io.File(_image!.path).readAsBytesSync();
-      // print(bytes);
-      // img = base64Encode(bytes);
-      // print(img);
+      img = 'data:image/$ext;base64,${base64Encode(fileBytes)}';
     } else {
       throw Exception("Error: _image is null");
     }
-
-    print(img);
-
-// final img
 
     try {
       final res = await http.post(
@@ -88,49 +68,68 @@ class _HomePageState extends State<HomePage> {
         body: jsonEncode({"image": img}),
       );
 
-      // final request = http.MultipartRequest('POST', Uri.parse(apiUrl));
-      // request.files.add(http.MultipartFile.fromBytes('image', imageBytes, filename: 'upload.jpg'));
-
-      // final response = await request.send();
       if (res.statusCode == 200) {
-        // final result = await res.stream.bytesToString();
         final data = jsonDecode(res.body);
-        print(data);
-
         String base64Image = data["image"].split(',').last;
-
-        // Decode the base64 string
         Uint8List bytes = base64Decode(base64Image);
 
-        // Display the image
-        // return;
+        List<String> faces = List<String>.from(data["recognized_faces"]);
+        faces.removeWhere((name) => name.trim().toLowerCase() == "unknown");
 
         setState(() {
-          recognizedFaces =
-              'Recognized faces: ${List<String>.from(data["recognized_faces"]).join(',')}';
+          recognizedFaces = 'Recognized faces: ${faces.isNotEmpty ? faces.join(', ') : "No known faces recognized"}';
+          recognizedNames = faces;
           output = bytes;
         });
-        print(recognizedFaces);
       } else {
-        print('Error: ${res.statusCode}');
         setState(() {
           errorMessage = 'Error: ${res.statusCode}';
         });
       }
     } catch (e) {
-      print('Error: $e');
       setState(() {
         errorMessage = 'Error: $e';
       });
     }
   }
 
+  Future<void> generateAndSavePdf(List<String> names, Uint8List image) async {
+    final pdf = pw.Document();
+    final imageProvider = pw.MemoryImage(image);
+
+    try {
+      final file = File('recognized_names.txt');
+      await file.writeAsString(names.join('\n'));
+    } catch (e) {
+      print('Error saving recognized names: $e');
+    }
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.ListView.builder(
+            itemCount: names.length,
+            itemBuilder: (context, index) => pw.Column(
+              children: [
+                pw.Text(names[index], style: pw.TextStyle(fontSize: 24)),
+                pw.Padding(padding: const pw.EdgeInsets.all(10)),
+                pw.Image(imageProvider, height: 200, width: 200),
+                //pw.Divider(color: PdfColors.black),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+
+    await Printing.sharePdf(bytes: await pdf.save(), filename: 'RecognizedFaces.pdf');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Smart Attendance System',
-            style: TextStyle(fontWeight: FontWeight.bold)),
+        title: Text('Smart Attendance System', style: TextStyle(fontWeight: FontWeight.bold)),
         centerTitle: true,
       ),
       body: Container(
@@ -145,12 +144,8 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('GIK Institute',
-                  style: TextStyle(
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white)),
-              const SizedBox(height: 40.0),
+              Text('GIK Institute', style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.white)),
+              SizedBox(height: 40.0),
               ElevatedButton(
                 onPressed: pickImage,
                 style: ElevatedButton.styleFrom(
@@ -160,31 +155,23 @@ class _HomePageState extends State<HomePage> {
                     borderRadius: BorderRadius.circular(20.0),
                     side: BorderSide(color: Colors.blue, width: 2.0),
                   ),
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
+                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
                   elevation: 5,
                 ),
-                child: Text('Pick Image',
-                    style:
-                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                child: Text('Pick Image', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
               ),
               SizedBox(height: 20.0),
-              // if (_image != null) Image.file(_image!, width: 200, height: 200),
-              const SizedBox(height: 20.0),
               if (recognizedFaces.isNotEmpty)
-                Text(recognizedFaces,
-                    style: TextStyle(color: Colors.white, fontSize: 16)),
+                Text(recognizedFaces, style: TextStyle(color: Colors.white, fontSize: 16)),
               if (errorMessage.isNotEmpty)
-                Text(errorMessage,
-                    style: TextStyle(color: Colors.red, fontSize: 16)),
-
+                Text(errorMessage, style: TextStyle(color: Colors.red, fontSize: 16)),
               if (output != null)
-                Image.memory(
-                  output!,
-                  fit: BoxFit.cover,
-                  height: 200,
-                  width: 200, // Adjust this as per your need
-                )
+                Image.memory(output!, fit: BoxFit.cover, height: 200, width: 200),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: output != null ? () => generateAndSavePdf(recognizedNames, output!) : null,
+                child: Text('Generate PDF'),
+              ),
             ],
           ),
         ),
